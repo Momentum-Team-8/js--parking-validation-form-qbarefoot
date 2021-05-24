a0efe2d4b82f1cae4b8c951f2562c1e996815e56
qbarefoot <quintenbarefoot@gmail.com>
2021-05-24 06:41:05 -0400

    an attempt was made for validation

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('Add validation!');
+tylconsole.log('Add validation!');
+
+document.getElementById('parking-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    let name = document.getElementsByTagName('name')[0];
+    
+    if name === '' {
+
+        let field = "name" ;
+        let id = "name" ;
+        let divname = "name-field" ;
+        emptynameerror()
+
+    }
+
+    let caryear = document.getElementsByTagName('car-year')[0];
+
+    if (caryear === '') {
+
+        let field = "car-year" ;
+        let id = "car year" ;
+        let divname = "car-field"
+        emptynamerror()
+
+    }
+
+    let carmake = document.getElementsByTagName('car-make')[0];
+
+    if (carmake === '') {
+
+        let field = 
+    }
